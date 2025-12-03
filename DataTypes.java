6863d772fb8ed585036e3508e9d68f3fce596050
brodykeeth <bdkeeth@gmail.com>
2025-12-03 13:31:58 +0000

    commit1

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+class  DataTypeExamples {
+
+    int myInt = 42;
+    double myDouble = 189.75;
+    char myChar = 'Q';
+    boolean myBool = false;
+
+}
+
+
+public class DataTypes {
+    public static void main(String[] args) {
+        
+        DataTypeExamples dt = new DataTypeExamples();
+        System.out.println("myInt: " + dt.myInt);
+    }
+
+}
